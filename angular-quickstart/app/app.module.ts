2cf11f25feb11d69d7a98c856dddbfcfcb9c87ce
klaus kuplen <klaus kuplen>
2016-10-09 17:39:03 +0200

    now with routing!

--- angular-quickstart/app/app.module.ts
+++ angular-quickstart/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heros.component';
+import {DashboardComponent} from './dashboard.component'
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    },
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
