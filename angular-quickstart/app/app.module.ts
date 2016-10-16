bbad8bc2002c7fe8f90ce9b2445fbfe3f68abd86
klaus kuplen <klaus kuplen>
2016-10-16 11:45:55 +0200

    now we use a mock-http service, yes!

--- angular-quickstart/app/app.module.ts
+++ angular-quickstart/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { RouterModule }  from '@angular/router';
+
+import { HttpModule }    from '@angular/http';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -14,6 +21,9 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+
     RouterModule.forRoot([
       {
         path: '',
--- angular-quickstart/app/heros.component.ts
+++ angular-quickstart/app/heros.component.ts
@@ -4,39 +4,58 @@ import { Router } from '@angular/router';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
-    @Component({
-        moduleId: module.id,
-        selector: 'my-heroes',
+@Component({
+  moduleId: module.id,
+  selector: 'my-heroes',
 
-      templateUrl : "heroes.component.html", 
-      styleUrls : ["heroes.component.css"],
+  templateUrl: "heroes.component.html",
+  styleUrls: ["heroes.component.css"],
 
-      providers: [HeroService]
+  providers: [HeroService]
 
-    })
-   export class HeroesComponent  implements OnInit {
-    title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
+})
+export class HeroesComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
 
-    constructor(
-      private router: Router,
-      private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
-    getHeroes(): void {
-          this.heroService.getHeroes().then(heros => this.heroes = heros)
-    }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heros => this.heroes = heros)
+  }
 
-    ngOnInit(): void {
-      this.getHeroes();
-    }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
 
-    onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-    }
+  delete(hero: Hero): void {
+  this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+}
 
-    gotoDetail(): void {
-      this.router.navigate(['/detail', this.selectedHero.id]);
-    }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
 
